a2ac4adcfba87601b2f7e816046858d7a752dd99
Steven Penny <svnpenn@gmail.com>
2016-11-20 16:46:50 -0600

    ./install.sh

--- ffmpeg/check.sh
+++ ffmpeg/check.sh
@@ -4,8 +4,3 @@
 /tmp/ffmpeg/ffmpeg -y -hide_banner -ss 00:27:00 \
 -i '/home/videos/breaking bad/season 3/breaking bad s3e12 half measures.mp4' \
 -t 10 outfile.mp4
-
-# mjpeg
-/tmp/ffmpeg/ffmpeg -y -hide_banner \
--i '/home/Music/Starcadian/Sgt. Tagowski-3380446717.mp3' \
--c copy outfile.mp3
