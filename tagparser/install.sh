4573c457c14178215d945125da892c610d342a2d
Steven Penny <svnpenn@gmail.com>
2016-11-20 18:30:30 -0600

    git clone git://github.com/Martchus/tagparser /tmp/tagparser

---
+++ tagparser/install.sh
@@ -0,0 +1,11 @@
+#!/bin/dash -e
+git clone git://github.com/Martchus/tagparser /tmp/tagparser
+cd /tmp/tagparser
+cmake -DCMAKE_AR=/bin/x86_64-w64-mingw32-ar \
+-DCMAKE_C_COMPILER=x86_64-w64-mingw32-gcc \
+-DCMAKE_CXX_COMPILER=x86_64-w64-mingw32-g++ \
+-DCMAKE_RC_COMPILER_INIT=x86_64-w64-mingw32-windres \
+-DCMAKE_FIND_ROOT_PATH=/usr/x86_64-w64-mingw32/sys-root/mingw \
+-DCMAKE_INSTALL_PREFIX=/usr/x86_64-w64-mingw32/sys-root/mingw \
+-DDISABLE_SHARED_LIBS=1 -DENABLE_STATIC_LIBS=1
+make install
